f98619f6d0c9886cecee45a5da07ad8cefeec3ba
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-07-25 20:55:39 +0530

    closures

--- closures.js
+++ closures.js
@@ -7,3 +7,18 @@ function printName() {
 printName();
 //closure - a function that remembers where it is created and has access to outer variables
 
+let num = 1;
+
+function outerFn() {
+  function innerFn() {
+    console.log(num);
+  }
+  return innerFn;
+}
+
+let fn = outerFn();
+
+fn();
+
+//Every running function has something called lexical environment
+//Lexical env has environment record for storing all objects/function declarations and reference to outer lexical environment
