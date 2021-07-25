4f5ac25678b4b0b9b05dd95026221e657e946935
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-07-25 20:51:08 +0530

    closures

---
+++ closures.js
@@ -0,0 +1,9 @@
+let client = "Thor";
+
+function printName() {
+  console.log(client);
+}
+
+printName();
+//closure - a function that remembers where it is created and has access to outer variables
+
