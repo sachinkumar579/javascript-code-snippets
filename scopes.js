4724cb8f7bb801d2964b2549b2e46207762b9b09
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-07-25 21:18:43 +0530

    making it clear

--- scopes.js
+++ scopes.js
@@ -1,3 +1,4 @@
+// Global scope
 const globalScopeC = "10";
 let globalScopeL = "learning javascript";
 var globalScopeV = "old school";
@@ -7,6 +8,7 @@ var globalScopeV = "old school";
 console.log(globalScopeC, globalScopeL, globalScopeV);
 // Prints all values
 
+// Block scope
 {
   const blockScopeC = "20";
   let blockScopeL = "learning javascript";
@@ -19,14 +21,21 @@ console.log(globalScopeC, globalScopeL, globalScopeV);
 console.log(blockScopeV);
 //We can access only var variables declared in block scope
 
-function sayHelloFn() {
-  const fucntionScopeC = "20";
+// Function scope
+function printValues() {
+  const functionScopeC = "20";
   let functionScopeL = "learning javascript";
   var functionScopeV = "old school";
 
-  console.log(fucntionScopeC, functionScopeL, functionScopeV);
+  console.log(globalScopeC, globalScopeL, globalScopeV);
+  console.log(functionScopeC, functionScopeL, functionScopeV);
   // Prints all values because a function has access to its outer variables  + variables declared within function scope.
+
+  //console.log(blockScopeC, blockScopeL, blockScopeV);
+  //If you uncomment this you will see an error because the visibility for these variables is only between line 10 to line 17
 }
+
+printValues();
 
 //console.log(fucntionScopeC, functionScopeL, functionScopeV);
 // No, you can't access funcion scoped variables. These variables are not accessible outside.
