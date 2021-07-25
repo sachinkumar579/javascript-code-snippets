ad99f66c2198834365e691e5999847eed81b9fb8
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-07-25 20:48:54 +0530

    refactor

--- scopes.js
+++ scopes.js
@@ -1,34 +1,32 @@
 const globalScopeC = "10";
-let globalScopeL ="learning javascript";
+let globalScopeL = "learning javascript";
 var globalScopeV = "old school";
 
 // Still using notepad at this point. Because I want to be a Javascript expert.
 
-console.log( globalScopeC , globalScopeL , globalScopeV );
+console.log(globalScopeC, globalScopeL, globalScopeV);
 // Prints all values
 
 {
-const blockScopeC = "20";
-let blockScopeL ="learning javascript";
-var blockScopeV = "old school";  
+  const blockScopeC = "20";
+  let blockScopeL = "learning javascript";
+  var blockScopeV = "old school";
 
-console.log( blockScopeC , blockScopeL , blockScopeV ) 
- // Prints perfectly on the console
+  console.log(blockScopeC, blockScopeL, blockScopeV);
+  // Prints perfectly on the console
 }
 
-console.log(blockScopeV)
-//We can access var variables declared in block scope
+console.log(blockScopeV);
+//We can access only var variables declared in block scope
 
 function sayHelloFn() {
-   const fucntionScopeC = "20";
-   let functionScopeL ="learning javascript";
-   var functionScopeV = "old school";  
+  const fucntionScopeC = "20";
+  let functionScopeL = "learning javascript";
+  var functionScopeV = "old school";
 
-   console.log("hello") 
-   console.log( c , l , v )
-   // Prints all values because a function has access to its outer variables  + variables declared within function scope.
-
+  console.log(fucntionScopeC, functionScopeL, functionScopeV);
+  // Prints all values because a function has access to its outer variables  + variables declared within function scope.
 }
 
-console.log( fucntionScopeC , functionScopeL , functionScopeV );
-// No, you can't access funcion scoped variables. These variables are not visible outside. 
+//console.log(fucntionScopeC, functionScopeL, functionScopeV);
+// No, you can't access funcion scoped variables. These variables are not accessible outside.
