43751c9168a42b2184ef5845aeded35525bc2918
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-08-21 22:57:45 +0530

    Update hoisting.js

--- hoisting.js
+++ hoisting.js
@@ -16,4 +16,16 @@ var x
 
 //This is called Variable Hoisting 
 
+// Function hoisting
+sayHi();
+
+function sayHi() {
+  console.log('hi');
+}
+
+// How does this work ? 
+// During the memory creation phase, JS engine recognized the function and hoisted it 
+// It made the function available by putting it in the memory . That's how sayHi function
+// was accessible prior to function declaration in execution phase 
+
 //Source https://codeburst.io/javascript-demystified-variable-hoisting-c3c4d2e8fd40
