25d151c24e1a72a698dc2c0095f3dde2a86a70fc
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-08-22 23:56:11 +0530

    Update executioncontext.js

--- executioncontext.js
+++ executioncontext.js
@@ -1,1 +1,9 @@
-// Following events happend when Javascript engine starts reading the code 
+// Following events happen as Javascript engine starts reading the code 
+
+// The global execution context is created before any code is executed
+
+// When a function is executed a new execution context is created
+
+// Every context provides this keyword which points to an object to which the current code that is executed belongs to 
+
+// Source https://codeburst.io/js-demystified-04-execution-context-97dea52c8ac6
