c84fbe09f89a3c8e569cde1102a14b55f9b45adb
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-08-30 00:17:03 +0530

    Update iife.js

--- iife.js
+++ iife.js
@@ -1,8 +1,9 @@
 // Immediately invoked function expression 
 
 let sayHello = ( function() { return 'hello' } ) ();
-
 console.log(sayHello)
+
+// Explanation - The code within () gets executed here. The code here is returning string 'hello' and that gets assigned to the variable sayHello
 
 // an even better version with arrow functions
 
