c9a3efa50d680f55f136200fd3d4ab41dbed2179
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-08-31 23:32:12 +0530

    Update async-await.js

--- async-await.js
+++ async-await.js
@@ -1,1 +1,29 @@
-// coming soon
+// A simplest definition I have found so far
+
+// Async makes a function return a Promise
+
+// Await makes a function wait for a Promise
+
+async function myFunction(){
+  return "hello"
+}
+
+//same as
+
+async function myFunction(){
+  return Promise.resolve("hello")
+}
+
+//How to use the Promise
+
+myFunction().then( 
+  function(value) { // code is successful } ,
+  function(error) { // code if there is an error }
+)
+
+// The word async before the function will make sure that the function
+// wil always return a Promise. All values will be wrapped in a Promise
+
+
+
+
