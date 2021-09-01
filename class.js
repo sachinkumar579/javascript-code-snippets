5555d364531129bbbd11ac8485115e14c25dcb38
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-01 22:25:51 +0530

    Update class.js

--- class.js
+++ class.js
@@ -4,17 +4,22 @@
 
  class ClassName {
    
-    constructor() {
-     // some code 
+    constructor(name) {
+       this.name = name
     }
    
-    method1() {
-     // do something
+    method1() { 
+       console.log('name is' +this.name)
     }
  }
 
  // Creating an object and calling the class methods
- let classObject = new ClassName();
+ let classObject = new ClassName('JSengine');
  classObject.method1();
+
+// When new new ClassName('JSengine') is called a new object is created and constructor code is executed
+// name is printed on calling method1() in line 18 . On creating the object in line 17 the value
+// JSengine is assigned to name and that is how the same value gets printed on calling method1() function
+
 
 
