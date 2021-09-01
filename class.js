7ad39b8625b57f2b067ac0dd13ed3f7e60a0c48f
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-01 21:30:55 +0530

    .

---
+++ class.js
@@ -0,0 +1,20 @@
+// Used for creating many objects of the same kind - employee, student, car 
+
+// Basic syntax
+
+ class ClassName {
+   
+    constructor() {
+     // some code 
+    }
+   
+    method1() {
+     // do something
+    }
+ }
+
+ // Creating an object and calling the class methods
+ let classObject = new ClassName();
+ classObject.method1();
+
+
