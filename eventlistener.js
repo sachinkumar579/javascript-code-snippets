e5c947b4b19ebd16efcf5449be79fb934854ff70
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-05 10:03:34 +0530

    Create eventlistener.js

---
+++ eventlistener.js
@@ -0,0 +1,8 @@
+let button = document.getElementById('button')
+
+button.addEventListener('click', function() { 
+  console.log('this is a click event')
+})
+
+// We are attaching an event to an element.
+// In the above code when the button is click line 4 will be printed in console 
