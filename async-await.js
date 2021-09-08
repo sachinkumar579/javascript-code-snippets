cd86a568c30b28f4084998cbb12c9d0d60c0cbac
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-08 21:46:04 +0530

    Update async-await.js

--- async-await.js
+++ async-await.js
@@ -39,3 +39,22 @@ async function sayHello() {
 console.log(sayHello()) // done will be printed after 3secs because setTimeout will resolve the promise after 3secs
     
 
+// Another example to explain async and await
+    
+// Since this function has the word async it will wrap the return object and return it as a Promise    
+async function fetchData() {
+    let resp = await fetch('https://reqres.in/api/users?page=2') // Returns a resolved promise on success 
+    return resp.json(); // returns the JSON data 
+}
+
+fetchData().then(user=>{   // then() method is for handling a successful response 
+    user.data.forEach(item => {  //Iterate through all items in data array and print the values 
+       console.log(item) 
+})});
+ 
+    
+// Here we are fetching JSON data using Fetch API 
+// JavaScript Fetch API can be used to make asynchronous HTTP requests. We can make HTTP requests to servers using Web browsers 
+// The fetch() method is available in the global scope that instructs the web browsers to send a request to a URL.
+// To get the actual data we can call one of the methods of the Response object - text() for raw data, json() for JSON data 
+    
