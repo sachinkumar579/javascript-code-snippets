ad157d48d91177c327023ea36f951feff3169191
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-09 22:14:21 +0530

    Create try-catch-finally.js

---
+++ try-catch-finally.js
@@ -0,0 +1,11 @@
+// Try Catch Finally statements are used for efficient handling of errors in code 
+// Basic syntax
+try {
+ // code to be executed that can result in errors
+
+ } catch(err) {
+   // handle the error 
+  } finally {
+    // runs irrespective of whether there is an error or not in try block 
+  }
+
