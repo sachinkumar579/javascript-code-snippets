5eb8577580d2aa4e72f85426d7e367bb5dc9ca8f
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-10 22:16:25 +0530

    Update data-types.js

--- data-types.js
+++ data-types.js
@@ -1,2 +1,15 @@
 // There are 8 basic data types in Javascript - number, string, bigint , boolean , undefined , null , symbol , object
 
+// Number - represents both integers and floating point numbers 
+// Bigint - represents integers of arbitrary length
+// String - a string must be surrounded by quotes " ' or ~
+// Boolean - The boolean type has only 2 values - true and false 
+// null - represents nothing or empty
+// undefined value is not assigned 
+// Object - All other types are called primitives because their values only contain a singel thing . Objects are used to store collections of data 
+
+// The typeof operator returns the type of the argument
+// Usage
+typeof(someVariable)
+typeof someVariable 
+
