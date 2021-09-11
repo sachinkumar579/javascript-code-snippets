f5ef308d891e7a6342c715b70270cac64360b5eb
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-12 00:01:27 +0530

    Update modules.js

--- modules.js
+++ modules.js
@@ -1,1 +1,4 @@
 // A module is a js file that can be loaded in any js file 
+// They can load each other and use directives like export and import to interchange functionality ,call functions of one module from another one 
+
+// Source https://javascript.info/modules-intro
