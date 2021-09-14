afcdc2676348520e07d2ce7ba42a0bd2db813aa8
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-14 21:04:44 +0530

    Create object-destructuring.js

---
+++ object-destructuring.js
@@ -0,0 +1,27 @@
+// ES 6 introduced object destructuring syntax that provides a way to assign properties of an object to variables
+
+// Syntax . Consider the below 
+
+let user = { name : 'sachin' , age : 100 } 
+
+let { name : name , age  : age } = user 
+
+console.log(name) // sachin
+
+console.log(age) // 100
+
+let { group : group } = user 
+
+// If the property doesn't exist the value will be undefined 
+
+console.log(group) // undefined 
+
+// Shorhand syntax
+
+let { name, age } = user
+
+console.log(name) // sachin
+
+console.log(age) // 100 
+
+
