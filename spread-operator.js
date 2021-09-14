6c1cb39f6198ff6adcfb570cce49b8ec3036956a
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-14 22:27:50 +0530

    Create spread-operator.js

---
+++ spread-operator.js
@@ -0,0 +1,17 @@
+// The spread operator ... is used to expand or spread an iterable or an array 
+
+// copy array using spread operator
+
+// clone array using spread operator 
+
+// spread operator with object 
+
+// Rest parameter 
+
+let data = [ 1 , 2 ]
+
+let arr = [ 3 , 4 ]
+
+let final = [...data , ...arr]
+
+console.log(final) // [1,2,3,4]
