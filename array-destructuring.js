73a7154b08f0ddc1124dc672cb2627f7e7f8961d
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-14 21:21:30 +0530

    Update array-destructuring.js

--- array-destructuring.js
+++ array-destructuring.js
@@ -10,6 +10,10 @@ console.log(one)  // 1
 
 console.log(two)  // 2
 
+let [one,,,four] = data
+
+console.log(four)  // 4 
+
 // The first variable one gets the first item 1 in the array ,  the second variable two gets the second item 2 in the array
 
 let [ one, two , three , four , five ] = data
@@ -21,3 +25,4 @@ let [ a , b ] = [ 1 , 2 ]
 console.log(a) // 1 
 
 console.log(b) // 2 
+
