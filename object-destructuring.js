8cbd164ca47cb408163f01aa896b2322a83305f5
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-14 21:09:01 +0530

    Update object-destructuring.js

--- object-destructuring.js
+++ object-destructuring.js
@@ -1,5 +1,5 @@
 // ES 6 introduced object destructuring syntax that provides a way to assign properties of an object to variables
-
+// We can extract data from arrays and objects and assign them to variables 
 // Syntax . Consider the below 
 
 let user = { name : 'sachin' , age : 100 } 
@@ -23,5 +23,6 @@ let { name, age } = user
 console.log(name) // sachin
 
 console.log(age) // 100 
+
 
 
