096500994bc1a48de8592153ce5281338cbdc4a4
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-17 23:30:23 +0530

    Update array-methods.js

--- array-methods.js
+++ array-methods.js
@@ -2,6 +2,13 @@ let arr = [1,2,3,4]
 
 console.log(arr)  // [1,2,3,4]
 
-arr.push(5) // adds items to the end 
+push() adds item to the end 
+ 
+arr.push(5) 
 
 console.log(arr) // [1,2,3,4,5]
+
+// adds items to the end and returns the length
+// pop() removes the last element of the array and returns that removed element
+// shift() removes the first element of the array and returns it
+
