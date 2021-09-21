9189119591266d6c2e4536f46bce8082b688b3e5
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-22 00:18:44 +0530

    Create mapsandset.js

---
+++ mapsandset.js
@@ -0,0 +1,11 @@
+// Map is a collection of keyed data items , Map allows keys of any type 
+
+let map = new Map()
+
+// A special feature about maps - objects can be used as keys 
+
+// Map preserves the insertion order unlike a regular object 
+
+// A set is a collection of unique values 
+
+let set = new Set()
