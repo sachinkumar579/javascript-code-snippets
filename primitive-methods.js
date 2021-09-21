f0d8b24a64e95499932ff5909ae9897c6d203e74
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-21 22:33:13 +0530

    Create primitive-methods.js

---
+++ primitive-methods.js
@@ -0,0 +1,7 @@
+// JS allows us to work with primitives as if they were objects 
+// There are 7 primitive types  - string, number , boolean , bigint , symbol , undefined , null 
+// A special object wrapper is created that provides extra functionalities and is then destroyed 
+// The object wrappers are different for each primitive types - Number ,String , Boolean , Symbol
+
+
+// Source https://javascript.info/primitives-methods
