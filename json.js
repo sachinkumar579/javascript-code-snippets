a831439fbfc2db065deaae4a63275ca544d8e707
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-24 23:56:37 +0530

    Update json.js

--- json.js
+++ json.js
@@ -1,7 +1,11 @@
-JSON is a data format that has its own independent standard and libraries for most programming languages.
-JSON supports plain objects, arrays, strings, numbers, booleans, and null.
-JavaScript provides methods JSON.stringify to serialize into JSON and JSON.parse to read from JSON.
-Both methods support transformer functions for smart reading/writing.
-If an object has toJSON, then it is called by JSON.stringify.
+// JSON is a data format that has its own independent standard and libraries for most programming languages.
+// JSON supports plain objects, arrays, strings, numbers, booleans, and null.
+// JavaScript provides methods JSON.stringify to serialize into JSON and JSON.parse to read from JSON.
+// Both methods support transformer functions for smart reading/writing.
+// If an object has toJSON, then it is called by JSON.stringify.
+
+let user =  { name : "Sachin" , age: 32}
+
+console.log(JSON.stringify(user)) // {"name":"Sachin","age":32}
 
 // Source https://javascript.info/json
