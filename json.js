eaf2db47f228133dad63d9a7212e5f6a912c2921
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-25 00:10:30 +0530

    Update json.js

--- json.js
+++ json.js
@@ -4,8 +4,29 @@
 // Both methods support transformer functions for smart reading/writing.
 // If an object has toJSON, then it is called by JSON.stringify.
 
+// JavaScript provides methods:
+
+// JSON.stringify to convert objects into JSON.
+// JSON.parse to convert JSON back into an object.
+
 let user =  { name : "Sachin" , age: 32}
 
 console.log(JSON.stringify(user)) // {"name":"Sachin","age":32}
+
+let user =  { name : "Sachin" , age: 32 , lazy : true , life: undefined, happiness: null}
+
+console.log(JSON.stringify(user)) // {"name":"Sachin","age":32,"lazy":true,"happiness":null}
+
+let user =  { name : "Sachin" , age: 32 , lazy : true , life: undefined, happiness: null , anarray : [1,2] , anotherobject: { a:1,b:2}}
+
+console.log(JSON.stringify(user)) // "name":"Sachin","age":32,"lazy":true,"happiness":null,"anarray":[1,2],"anotherobject":{"a":1,"b":2}}
+
+
+let jsonUser = '{"name":"Sachin","Age":31}'
+
+console.log(jsonUser) // {"name":"Sachin","Age":31}
+
+console.log(JSON.parse(jsonUser)) // {name: 'Sachin', Age: 31}
+
 
 // Source https://javascript.info/json
