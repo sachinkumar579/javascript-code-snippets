00c75d4656d79de4562d61d274d5ecc6959f8e89
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-25 14:21:36 +0530

    Update Comparisons.js

--- Comparisons.js
+++ Comparisons.js
@@ -14,5 +14,30 @@ console.log('1' == 1 ) // true
 
 console.log('1' == 2 ) // false
 
+// For boolean values true becomes 1 and false becomes 0 
+
+console.log(true == 1 ) // true
+
+console.log(true == 2) // false
+
+console.log(true == "1") // true 
+
+console.log(false == "") // true 
+console.log(false == 0) // true 
+console.log(false == "0") // true 
+
+// operands of different types are converted to numbers by the equality operator ==
+// A strict equality operator === checks the equality without type conversion
+
+// For maths and other comparisons < > <= >=
+// null/undefined are converted to numbers: null becomes 0, while undefined becomes NaN.
+
+console.log(1+null) // 1
+
+console.log(1+undefined) // NaN 
+
+console.log(null==0) // false 
+//equality check == and comparisons > < >= <= work differently.
+
 
 
