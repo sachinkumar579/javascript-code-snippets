5f947df67a8c3bfae1b999f81143e535dc0b5595
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-25 14:44:09 +0530

    Create GlobalObject.js

---
+++ GlobalObject.js
@@ -0,0 +1,9 @@
+// Global object provide variables and functions that are accessible anywhere . By default they are built into the language or the environment
+
+// In a browser it is named window and in nodejs environment it is called global 
+
+// In a browser, global functions and variables declared with var (not let/const!) become the property of the global object: 
+
+// The same effect have function declarations (statements with function keyword in the main code flow, not function expressions).
+
+// Source https://javascript.info/global-object 
