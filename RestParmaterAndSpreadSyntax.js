805b8184d90eea94f9c19dffda7a672d4c638cf9
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-25 15:24:08 +0530

    Update RestParmaterAndSpreadSyntax.js

--- RestParmaterAndSpreadSyntax.js
+++ RestParmaterAndSpreadSyntax.js
@@ -27,4 +27,34 @@ print(1) // prints 1
 print(1,2) // prints 1 2
 print(1,2,3) // prints 1,2,3 
 
+// The rest parameters must be at the end 
+function fuctionName(arg1,arg2.... , ...args) // args here will be an array 
 
+// ... rest must always be the last 
+
+// Rest allows us to get an array from a list of parameters 
+
+// When ...arr is used in the function call, it “expands” an iterable object arr into the list of arguments.
+
+let arr = [3,2]
+
+function add(a,b,c){
+    console.log(a+b+c)
+}
+
+add(1,2,...arr) // 1 will be assigned to a, 2 will be assigned to b , 3 from arr[0] will be assigned to c , 2 from arr[1] will be ignored 
+
+// When ... is at the end of function parameters, it’s “rest parameters” and gathers the rest of the list of arguments into an array.
+// When ... occurs in a function call or alike, it’s called a “spread syntax” and expands an array into a list.
+  
+let arr = [3,2]
+
+let arrCopy= [1,arr] 
+
+console.log(arrCopy) // arrCopy[0] = 1 , arrCopy[1] = arr (the array itself is stored in index position 1 )
+
+arrCopy = [ 1,...arr]
+
+console.log(arrCopy) // arrCopy[0] = 1 , arrCopy[1] = 3 , arrCopy[2] = 2 
+
+// Source  https://javascript.info/rest-parameters-spread
