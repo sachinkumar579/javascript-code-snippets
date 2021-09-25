e0874d2c5da00f22df9d8d0d6ac4963fe9b6dc1c
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-25 15:08:45 +0530

    Create RestParmaterAndSpreadSyntax.js

---
+++ RestParmaterAndSpreadSyntax.js
@@ -0,0 +1,30 @@
+// A function can be called with any number of arguments , there will be no error because of excessive arguments 
+
+let sum = 0 ;
+
+function add(a,b) 
+{
+   sum = a+b;
+}
+
+add(1,2,3,3,4)
+
+console.log(sum) // 3  . 3,3,4 will be ignored
+
+
+// Using the rest operator 
+
+function print(...arr) { // the dots ... here mean gather all parameters into an array 
+
+    for(let val in arr) {
+       console.log(val) 
+    }
+  
+}
+  
+
+print(1) // prints 1 
+print(1,2) // prints 1 2
+print(1,2,3) // prints 1,2,3 
+
+
