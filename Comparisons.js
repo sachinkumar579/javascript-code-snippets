b24086cd5cfb3bc344026a46d89ea75604a5600b
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-25 14:07:59 +0530

    Create Comparisons.js

---
+++ Comparisons.js
@@ -0,0 +1,18 @@
+// To compare strings Javascript uses Unicode order . Strings are compared letter by letter
+
+let result = "sachin" > "kumar"
+
+console.log(result) // true
+
+result = "A" > "a" 
+
+console.log(result) // false 
+
+// When comparing values for different types JS converts the values to numbers
+
+console.log('1' == 1 ) // true 
+
+console.log('1' == 2 ) // false
+
+
+
