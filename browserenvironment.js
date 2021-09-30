01a4587c89f963b1a82f381af48b6d0e854a11d0
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-09-30 23:49:52 +0530

    Create browserenvironment.js

---
+++ browserenvironment.js
@@ -0,0 +1,7 @@
+// Document object model or DOM  represents all page contents as objects that can be modified 
+
+// The DOM specification explains the structure of the document and provides objects to manipulate it 
+
+// Browser object model or BOM represents additional objects provided by the browser environment for working with everything except the document
+
+// Functions alert,confirm,prompt are also part of BOM 
