d59c46a8f47ddf322df3d624061b77a802b58298
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-10-01 23:41:18 +0530

    Create DomTree.js

---
+++ DomTree.js
@@ -0,0 +1,13 @@
+// The backbone of an HTML document is tags. According to the Document Object Model (DOM), every HTML tag is an object All these objects are accessible using JavaScript
+
+// For example, document.body is the object representing the <body> tag. 
+
+// DOM represents HTML as a tree structure of tags.
+
+// the top tag is always <html>. Even if it doesn’t exist in the document, it will exist in the DOM, because the browser will create it.
+
+// The document object that represents the whole document is, formally, a DOM node as well.
+
+// $ ( insert a number).methods()  - get a node from elements in console 
+
+// We can use developer tools to inspect DOM and modify it manually.
