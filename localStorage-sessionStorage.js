6defe7ef3c2a377840241cab6f4426ebb220b89d
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-12-18 16:21:38 +0530

    Create localStorage-sessionStorage.js

---
+++ localStorage-sessionStorage.js
@@ -0,0 +1,13 @@
+// Saving data in the browser 
+
+// sessionStorage  accessible - same tab , expires on - tab close , storage location - browser 
+
+// localStorage  accessible - Any window , expires on - Never , storage location - browser 
+
+//The data is saved locally only and can’t be read by the server, which eliminates the security issue that cookies present. Everything’s done in JavaScript.
+
+// localStorage
+// Data is shared between all tabs and windows from the same origin.
+// The data will not expire. It will remain even after browser restart and survive OS reboot too.
+
+// Source https://javascript.plainenglish.io/localstorage-vs-sessionstorage-c22164aff067
