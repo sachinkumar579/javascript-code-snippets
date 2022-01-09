119f947ff6f04f5dd796449352a326cf67349f68
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2022-01-10 01:04:42 +0530

    Create design-patterns.js

---
+++ design-patterns.js
@@ -0,0 +1,2 @@
+// The ultimate goal when we write code should be make it maintainable , readbable and reusable  
+// Design patterns are reusable templates for commonly occurring problems in software design . 
