4457e2d9f5abd271488314d8adfb8d7debdc1b84
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2022-01-10 16:22:49 +0530

    Update spread-operator.js

--- spread-operator.js
+++ spread-operator.js
@@ -15,3 +15,18 @@ let arr = [ 3 , 4 ]
 let final = [...data , ...arr]
 
 console.log(final) // [1,2,3,4]
+
+// A very strange problem 
+const user = {
+  name:'sachin',
+  location : {
+    city:'Delhi',
+    country:'India'
+  }
+};
+
+const copy = {...user}
+copy.name="Mang"
+
+console.log(user,copy)
+
