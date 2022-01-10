b2cc21c63f0336f3b75fae86d1b98109b62a73cb
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2022-01-10 13:00:17 +0530

    Update design-patterns.js

--- design-patterns.js
+++ design-patterns.js
@@ -1,2 +1,16 @@
-// The ultimate goal when we write code should be make it maintainable , readbable and reusable  
+// The ultimate goal when we write code should be make it maintainable , readbable and reusable , no matter which programming language is used   
 // Design patterns are reusable templates for commonly occurring problems in software design . 
+// solutions for repeated problems 
+
+// Creational - how to create objects 
+// Structural -  relationships between objects
+// Behavior - communication/interaction between objects 
+
+// OOPs concepts
+// Encapsulation  - having the variables and methods in a unit called Class and allowing only the Class to update variables using methods . 
+// The class can entirely control how the state must be altered 
+// Abstraction - hide certain details and show only what is important in the object . 100 % abstraction can be achieved using interfaces . 
+// We can include only those which are to be exposed to the caller . The class implementing this interface can have more methods which are not directly exposed to the caller
+// Another simple way to achieve abstraction is by making methods private . Ex If there is ConnectServer class ,  we can make initiate() , authenticate() methods
+// private and only expose one method called connect by making it public 
+// 
